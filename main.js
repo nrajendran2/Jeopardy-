c9ed45065a96063cc8edd7b4870c65d69f17096e
rajendranneil19 <rajendranneil19@gmail.com>
2018-02-06 10:40:54 -0500

    Changed color scheme and got logic for first question, not as dynamic yet. Just looking for MVP

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-alert("Hello world")
+
+//alert ("Hello World")
+
+let totalPoints = 0;
+$(() => {
+    $('#one').click(function () {
+        console.log("this worked")
+        let answer = prompt("Quarterback whom has won 5 Super Bowls with the Patriots  _____.")
+        if (answer === "Tom Brady" || answer === "tom brady") {
+            alert("Correct, +100")
+            totalPoints += 100;
+            $('.playerscore').html(`Score: ${totalPoints}`)
+        } else {
+            alert("Sorry, wrong answer -100")
+            totalPoints -= 100;
+            $('.playerscore').html(`Score: ${totalPoints}`)
+        }
+    })
+})
+
+$('#two').click(function () {
+    console.log("this worked")
+    let answer = prompt("The winner of the 2010 World Cup ")
+    if (answer === "Spain" || answer === "spain" || answer === "Espana") {
+        alert("Correct, +200")
+        totalPoints += 200;
+        $('.playerscore').html(`Score: ${totalPoints}`)
+    } else {
+        alert("Sorry, wrong answer -200")
+        totalPoints -= 200;
+        $('.playerscore').html(`Score: ${totalPoints}`)
+    }
+})
